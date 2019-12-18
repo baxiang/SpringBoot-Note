c6e71feca8feda1e41a3814b6e6a442bc48e4ae2
baxiang <yangyucug@gmail.com>
2019-12-19 01:51:22 +0800

    mybatic

--- HelloWorld/src/main/java/com/github/controller/HelloController.java
+++ HelloWorld/src/main/java/com/github/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("hello")
     public String sayHello() {
-        return "Hello Spring boot\n";
+        return "Hello Spring Boot";
     }
 }
